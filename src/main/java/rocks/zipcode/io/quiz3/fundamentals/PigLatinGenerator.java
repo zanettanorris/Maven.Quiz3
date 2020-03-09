05d627318a4d30e2fb71dfdcedab9afff3dcde74
Zanetta Norris <zanettanorris@gmail.com>
2020-03-09 12:33:35 -0400

    added more fundamentals.

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -5,6 +5,17 @@ package rocks.zipcode.io.quiz3.fundamentals;
  */
 public class PigLatinGenerator {
     public String translate(String str) {
+//        StringBuilder sb = new StringBuilder();
+
+//        if (VowelUtils.startsWithVowel(str)) {
+//            String pig = str + "way";
+//            for (int i = 0; i < str.length(); i++)
+//                while (!VowelUtils.isVowel(str.charAt(i))) {
+//
+//                }
+//        }
+//        return sb.toString();
         return null;
     }
 }
+
--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
@@ -8,18 +8,36 @@ import java.util.List;
  */
 public class StringUtils {
     public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
-        return null;
+        StringBuilder sb = new StringBuilder(str);
+        if (indexToCapitalize < str.length()) {
+            sb.setCharAt(indexToCapitalize, Character.toUpperCase(sb.charAt(indexToCapitalize)));
+        }
+        return sb.toString();
     }
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
-        return null;
+        if (baseString.charAt(indexOfString) == characterToCheckFor) {
+            return true;
+        } else return false;
     }
 
     public static String[] getAllSubStrings(String string) {
-        return null;
-    }
 
-    public static Integer getNumberOfSubStrings(String input){
-        return null;
+        String[] result = new String[getNumberOfSubStrings(string)];
+        for (int j = 0; j<= getNumberOfSubStrings(string); j++) {
+                for (int i = 0; i < (string.length()); i++) {
+                  for (int len = 1; len < (string.length()); len++){
+                     result[j] = string.substring(i, len);
+                }
+            }
+        }
+        return result;
+    }
+    public static Integer getNumberOfSubStrings(String input) {
+        Integer len = input.length();
+        for (int n = input.length()-1; n > 1; n--) {
+            len = len + n;
+        }
+        return len;
     }
 }
